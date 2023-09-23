bfae27e55ff683522ef5969a1ac5d454a0f2a14c
Shanto57575 <shanto57575@gmail.com>
2023-09-24 02:57:49 +0600

    added spinner

--- src/components/AllDoctors/AllDoctors.jsx
+++ src/components/AllDoctors/AllDoctors.jsx
@@ -1,3 +1,4 @@
+import Loader from "../Loader/Loader.jsx";
 import { useEffect, useRef, useState } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { BsFlag } from "react-icons/bs";
@@ -7,10 +8,12 @@ import { Link } from "react-router-dom";
 const AllDoctors = () => {
 	const [allDoctors, setAllDoctors] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
+	const [loading, setLoading] = useState(true);
 	const [search, setSearch] = useState("");
 	const [gender, setGender] = useState("");
 	const [speciality, setSpeciality] = useState("");
 	const [fees, setFees] = useState("");
+
 	const searchRef = useRef(null);
 	const genderRef = useRef(null);
 	const specialityRef = useRef(null);
@@ -36,7 +39,10 @@ const AllDoctors = () => {
 			`http://localhost:5000/alldoctors?page=${currentPage}&itemsPerPage=${itemsPerPage}&search=${search}&gender=${gender}&speciality=${speciality}&fees=${fees}`
 		)
 			.then((res) => res.json())
-			.then((data) => setAllDoctors(data));
+			.then((data) => {
+				setAllDoctors(data);
+				setLoading(false);
+			});
 	}, [currentPage, search, gender, speciality, fees]);
 
 	console.log(allDoctors);
@@ -140,96 +146,105 @@ const AllDoctors = () => {
 						</div>
 					</div>
 				</div>
-				{allDoctors.map((doc) => (
-					<div
-						key={doc._id}
-						className="flex items-center flex-wrap justify-between hover:shadow-2xl shadow-slate-800 mb-5 border rounded-xl"
-					>
-						<div className="w-full lg:w-[37%] space-x-2 space-y-3">
-							<div className="flex flex-wrap items-center">
-								<img
-									className="md:w-28 md:h-28 border md:m-3 shadow-md shadow-slate-400 rounded-xl"
-									src={doc.picture}
-									alt="Album"
-								/>
-								<div className="text-left border-l-4 md:border-l-0 border-cyan-600 pl-3 mt-4 md:mt-0">
-									<p className="badge bg-blue-400 text-white">
-										{doc.experience}+
-									</p>
-									<p className="text-cyan-500 font-semibold">
-										{doc.speciality}
-									</p>
-									<p className="text-lg font-bold">{doc.name}</p>
-									<p className="text-sm text-slate-500">{doc.education}</p>
+				{loading ? (
+					<Loader />
+				) : totalItems === 0 ? (
+					<p className="text-5xl text-cyan-500">
+						<span className="text-red-600 block">X</span>
+						No doctors found.
+					</p>
+				) : (
+					allDoctors.map((doc) => (
+						<div
+							key={doc._id}
+							className="flex items-center flex-wrap justify-between hover:shadow-2xl shadow-slate-800 mb-5 border rounded-xl"
+						>
+							<div className="w-full lg:w-[37%] space-x-2 space-y-3">
+								<div className="flex flex-wrap items-center">
+									<img
+										className="md:w-28 md:h-28 border md:m-3 shadow-md shadow-slate-400 rounded-xl"
+										src={doc.picture}
+										alt="Album"
+									/>
+									<div className="text-left border-l-4 md:border-l-0 border-cyan-600 pl-3 mt-4 md:mt-0">
+										<p className="badge bg-blue-400 text-white">
+											{doc.experience}+
+										</p>
+										<p className="text-cyan-500 font-semibold">
+											{doc.speciality}
+										</p>
+										<p className="text-lg font-bold">{doc.name}</p>
+										<p className="text-sm text-slate-500">{doc.education}</p>
+									</div>
+								</div>
+								<p className="text-left ml-4 text-md text-cyan-600 font-extrabold">
+									{doc.service}
+								</p>
+							</div>
+							<div className="w-full lg:w-[33%] border-x-4 border-cyan-600 pl-4 flex flex-col justify-end space-y-1 py-3">
+								<div className="flex items-center gap-2">
+									<span className="text-cyan-600">
+										<BsFlag size={18} />
+									</span>
+									{doc.country}
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="text-cyan-600">
+										<AiOutlineClockCircle size={20} />
+									</span>
+
+									{doc.availability}
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="text-cyan-600">
+										<HiMailOpen size={20} />
+									</span>
+
+									{doc.contact}
+								</div>
+								<div className="flex items-center gap-2 text-cyan-500 font-extrabold">
+									<span className="text-cyan-600">
+										<HiOutlineCurrencyDollar size={20} />
+									</span>
+									{doc.fees}
+								</div>
+								<div className="flex items-center">
+									<button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+										<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+											View Full Profile
+										</span>
+									</button>
+
+									<button className="border-2 hover:border-red-600 duration-300 hover:text-red-600 relative inline-flex items-center justify-center px-1 rounded-md mb-2 mr-2 text-sm font-medium">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="w-10 h-10"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+											/>
+										</svg>
+									</button>
 								</div>
 							</div>
-							<p className="text-left ml-4 text-md text-cyan-600 font-extrabold">
-								{doc.service}
-							</p>
-						</div>
-						<div className="w-full lg:w-[33%] border-x-4 border-cyan-600 pl-4 flex flex-col justify-end space-y-1 py-3">
-							<div className="flex items-center gap-2">
-								<span className="text-cyan-600">
-									<BsFlag size={18} />
-								</span>
-								{doc.country}
-							</div>
-							<div className="flex items-center gap-2">
-								<span className="text-cyan-600">
-									<AiOutlineClockCircle size={20} />
-								</span>
-
-								{doc.availability}
-							</div>
-							<div className="flex items-center gap-2">
-								<span className="text-cyan-600">
-									<HiMailOpen size={20} />
-								</span>
-
-								{doc.contact}
-							</div>
-							<div className="flex items-center gap-2 text-cyan-500 font-extrabold">
-								<span className="text-cyan-600">
-									<HiOutlineCurrencyDollar size={20} />
-								</span>
-								{doc.fees}
-							</div>
-							<div className="flex items-center">
-								<button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-									<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-										View Full Profile
-									</span>
-								</button>
-
-								<button className="border-2 hover:border-red-600 duration-300 hover:text-red-600 relative inline-flex items-center justify-center px-1 rounded-md mb-2 mr-2 text-sm font-medium">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={1.5}
-										stroke="currentColor"
-										className="w-10 h-10"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-										/>
-									</svg>
-								</button>
+							<div className="gap-2 w-full lg:w-[20%] space-x-2 space-y-3 border-cyan-600">
+								<Link to={`/appointmentForm/${doc._id}`}>
+									<button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+										<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+											Book Your Appointment
+										</span>
+									</button>
+								</Link>
 							</div>
 						</div>
-						<div className="gap-2 w-full lg:w-[20%] space-x-2 space-y-3 border-cyan-600">
-							<Link to={`/appointmentForm/${doc._id}`}>
-								<button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-									<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-										Book Your Appointment
-									</span>
-								</button>
-							</Link>
-						</div>
-					</div>
-				))}
+					))
+				)}
 				<div>
 					<p className="text-cyan-500 mb-2 font-extrabold text-2xl">
 						Page : {currentPage}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+	return (
+		<div className="text-7xl text-blue-400">
+			L
+			<span className="inline-block animate-spin w-12 h-12 border-8 border-blue-400 border-dashed rounded-full"></span>
+			ading....
+		</div>
+	);
+};
+
+export default Loader;
